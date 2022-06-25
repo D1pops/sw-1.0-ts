859bdd80be67f30274e6f4a1f0e472b53d2092ec
D1pops <dipops@yande.ru>
2022-06-25 18:12:07 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        Привет Самурай, погнали!
+    </div>
+  );
+}
+
+export default App;
